b553e273c6a8acc9d8feceac75202bfb6ca7cc3b
dheerajneelam-sigfyn <dheeraj.neelam@sigfyn.com>
2022-02-17 11:52:04 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@ export default function App() {
   const myInit = {
     headers: {},
   };
-  console.log('Test');
+  console.log('Test11');
   Amplify.configure(awsmobile);
   const [clicked, setClicked] = useState(0);
   const [apiMessage, setApiMessage]: any = useState();
